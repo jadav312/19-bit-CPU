d07fc06c0d96d29337b4d9311601d4626aefeebe
jadav312 <147308422+jadav312@users.noreply.github.com>
2025-05-23 12:57:12 +0530

    Update data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -1,18 +1,27 @@
-module memory (
-    input clk,
-    input [18:0] addr,
-    input [18:0] write_data,
-    input mem_write,
-    input mem_read,
-    output reg [18:0] read_data
-);
-
-  reg [18:0] mem [0:511]; // Small memory
-
-  always @(posedge clk) begin
-    if (mem_write)
-      mem[addr] <= write_data;
-    if (mem_read)
-      read_data <= mem[addr];
-  end
-endmodule
+    module data_memory (input clk,
+                        input [18:0] addr,
+                        input [18:0] write_data,
+                        input mem_write,
+                        input mem_read,
+                        output reg [18:0] read_data
+                        );
+    
+                    reg [18:0] mem [0:511];                 //small data memory to handle internal data while doing decode, execure addr
+                    
+                    always @(posedge clk ) begin
+                            if(mem_write ) begin
+                                mem [ addr ] <= write_data ;
+                                
+                                end else if(mem_read ) begin
+                                    read_data <= mem[addr];
+                                end
+                    
+                    end
+                    
+    
+    
+    
+    
+    
+    
+    endmodule 
