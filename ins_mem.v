f92d34e5714ccd5617ac0496263d17e41efa4b9e
jadav312 <147308422+jadav312@users.noreply.github.com>
2025-05-23 12:57:45 +0530

    Update ins_mem.v

--- ins_mem.v
+++ ins_mem.v
@@ -1,21 +1,25 @@
-module ins_mem (input clk,
-		input ins_mem_WE,
-		input [18:0] data_IM,
-		input [15:0] address_IM,
-		output reg [18:0] out_IM 
-		);
-
-
-		reg [18:0] mem [0:65535];
-
-		always@(posedge clk) begin
-		if(ins_mem_WE == 1) begin
-			mem[address_IM] <= data_IM;
-
-		end else begin
-			out_IM <= mem[address_IM];
-		end
-		end
-
-
-endmodul
+module Ins_mem( input clk,
+                input ins_mem_WE,
+                input [16:0] address_IM,
+                input [18:0] data_IM,
+                output reg [18:0] out_IM
+                );
+                
+                
+                reg [18:0] mem [0:65535] ;      // 19 bit reg to store address and data
+                
+                always @(posedge clk) begin
+                
+                        if(ins_mem_WE == 1) begin
+                            mem[address] <= data_IM ;
+                            
+                        end else begin
+                            out_IM <= mem[address];
+                            
+                            
+                            
+                            
+                            
+                 end    
+                        end 
+endmodule
